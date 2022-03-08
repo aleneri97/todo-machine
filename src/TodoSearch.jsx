3a8afbe115ff664bdb9742b3604f8e66175757fc
Manuel Neri <ale_neri@yahoo.com>
2022-03-07 18:04:44 -0600

    ✨  [ADD] State handler

--- src/TodoSearch.jsx
+++ src/TodoSearch.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import './TodoSearch.scss';
 
 export function TodoSearch() {
+	const [searchValue, setSearchValue] = React.useState(''); // Hook: returns a value and a function to update it
+
 	const onSearchValueChange = (e) => {
+		setSearchValue(e.target.value);
 		console.log(e.target.value);
 	};
 
-	return (
+	return [
 		<input
 			type='search'
 			placeholder='Buscar TODO'
+			value={searchValue}
 			onChange={onSearchValueChange}
-		/>
-	);
+		/>,
+		<p>{searchValue}</p>,
+	];
 }
