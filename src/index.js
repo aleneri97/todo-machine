34b24d974eca1c77cd000c1e73dc3c2e4ea00805
Alejandro Neri <ale_neri@yahoo.com>
2022-03-14 17:58:15 -0600

    🚀  Deploy project v1

--- src/index.js
+++ src/index.js
@@ -3,4 +3,9 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 
+// TODO: Modal Background clickeable para cerrar modal
+// TODO: Editar TODOs
+// TODO: Avance como barra de progreso con porcentaje
+// TODO: Esconder hechos
+
 ReactDOM.render(<App />, document.getElementById('root'));
