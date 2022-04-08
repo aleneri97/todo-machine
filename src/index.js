7f498e4e73054617b28f6cdf686ec50129300984
Alejandro Neri <ale_neri@yahoo.com>
2022-04-08 18:51:22 -0500

    💩 [MOCK] Component to teach High Order Components

--- src/index.js
+++ src/index.js
@@ -1,11 +1,33 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 
 // TODO: Modal Background clickeable para cerrar modal
 // TODO: Editar TODOs
 // TODO: Avance como barra de progreso con porcentaje
 // TODO: Esconder hechos
 
-ReactDOM.render(<App />, document.getElementById('root'));
+function AppComponent(props) {
+	return (
+		<h1>
+			¡{props.saludo}, {props.nombre}!
+		</h1>
+	);
+}
+
+function wrappedComponent(WrappedComponent) {
+	return function withGreeting(saludo) {
+		return function ActualComponent(props) {
+			return (
+				<React.Fragment>
+					<WrappedComponent {...props} saludo={saludo} />
+					<p> Esto es una función de verdad </p>
+				</React.Fragment>
+			);
+		};
+	};
+}
+
+const App = wrappedComponent(AppComponent)('Hola');
+
+ReactDOM.render(<App nombre='Juanita' />, document.getElementById('root'));
